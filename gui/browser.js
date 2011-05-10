24838a35d274831647fe5d9cac6450a53cd0da2b
David Taylor <dltaylor@andrew.cmu.edu>
2011-05-10 04:20:08 -0400

    initial checkin

---
+++ gui/browser.js
@@ -0,0 +1,49 @@
+function playSegment(x, d) {
+	return function() {
+		$("#frames").fadeOut();
+		$("#video").fadeIn();
+		$("#player").get(0).currentTime = x;
+		$("#player").get(0).play();
+		tm = setTimeout(function() {
+			$("#player").get(0).pause();
+			$("#video").fadeOut();
+			$("#frames").fadeIn();
+		}, 6000);
+		$("#playon").show();
+	}
+}
+
+$(document).ready(function() {
+	$("#shutup").hide();
+	$("#video").hide();
+
+	$("#playon").click(function() {
+		clearTimeout(tm);
+		$("#playon").hide();
+		$("#shutup").show();
+
+	});
+	$("#shutup").click(function() {
+		$("#playon").show();
+		$("#shutup").hide();
+		$("#player").get(0).pause();
+		$("#video").fadeOut();
+		$("#frames").fadeIn();
+	});
+	
+	for(i in image_list){
+		var newDiv= $('<div class="frame"></div>')
+		var play = $('<a class="ts" href="#">'+i*image_rate+'</a>')
+		play.click(playSegment((i-1)*image_rate-2));
+		newDiv.append(play);
+
+		var img = $('<img src="images/'+image_list[i]+'" alt="video frame" />')
+		img.toggle(
+			function(){$(this).parent().addClass("zoomed").parent().addClass("zoom").removeClass("preview") },
+			function(){$(this).parent().removeClass("zoomed").parent().removeClass("zoom").addClass("preview") }
+		);
+		newDiv.append(img);
+		$("#frames").append(newDiv);
+	}
+
+});
---
+++ scripts/process.sh
@@ -0,0 +1,20 @@
+
+mkdir -p output
+rm -rf output/*.*
+
+cp gui/* output/
+
+mkdir -p output/images
+
+size="ntsc"
+rate=".2"
+
+ffmpeg -i $1 -f webm -s $size output/video.webm
+ffmpeg -i $1 -r $rate -s $size -f image2 output/images/%02d.jpg
+
+
+echo "image_list = Array();" > output/image_list.js
+echo "image_rate = 1 / $rate" >> output/image_list.js
+for i in `ls output/images`; do
+	echo "image_list.push('$i'); " >> output/image_list.js
+done
